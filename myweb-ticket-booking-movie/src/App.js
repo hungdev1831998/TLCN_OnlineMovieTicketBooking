ed1810ae4959c6f33b6cd43b08ab8133634b8042
hungdev1831998 <hungdev1831998@gmail.com>
2019-10-31 20:08:55 +0700

    update routes, structure component frontend

--- myweb-ticket-booking-movie/src/App.js
+++ myweb-ticket-booking-movie/src/App.js
@@ -5,7 +5,7 @@ import Register from "./components/Register/Register";
 import Header from "./components/header/header";
 import Footer from "./components/footer/footer";
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-
+import RouterWeb from './routes';
 
 
 class App extends React.Component {
@@ -13,10 +13,9 @@ class App extends React.Component {
   render() {
     return (
       <Router>
-           <Route exact path='/' component={Header}/> 
-           <Route exact path='/' component={Footer}/>
-           <Route exact path='/login' component={Login} />
-           <Route exact path='/Register' component={Register} />
+         <Header/>
+         <RouterWeb/>
+         <Footer/>
       </Router>
     );
   }
